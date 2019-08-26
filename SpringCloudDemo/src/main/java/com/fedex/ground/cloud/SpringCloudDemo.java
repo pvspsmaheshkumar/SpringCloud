d314599e0981f300510782aacc379b36000a3403
5099208 <5099208@PGH-L64966NB.corp.ds.fedex.com>
2019-08-25 23:39:05 -0400

    Changes to enable ZuulProxy

--- SpringCloudDemo/src/main/java/com/fedex/ground/cloud/SpringCloudDemo.java
+++ SpringCloudDemo/src/main/java/com/fedex/ground/cloud/SpringCloudDemo.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableEurekaClient
+@EnableZuulProxy
 public class SpringCloudDemo {
 	
 	@Bean
