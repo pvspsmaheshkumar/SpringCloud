4674b5393b3919f8793e557b3dd7d0f83f234cec
5099Ena b208 <5099208@PGH-L64966NB.corp.ds.fedex.com>
2019-08-26 04:45:10 -0400

    Enable Hystrix

--- SpringCloudDemo/src/main/java/com/fedex/ground/cloud/SpringCloudDemo.java
+++ SpringCloudDemo/src/main/java/com/fedex/ground/cloud/SpringCloudDemo.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -15,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @EnableZuulProxy
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class SpringCloudDemo {
 	
 	@Bean
