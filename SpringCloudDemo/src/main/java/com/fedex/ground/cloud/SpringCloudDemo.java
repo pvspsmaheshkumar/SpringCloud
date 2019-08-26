6c74f29d0e716536d2b7a3d0c757e6a8dba13363
5099208 <5099208@PGH-L64966NB.corp.ds.fedex.com>
2019-08-25 21:51:19 -0400

    Updates

--- SpringCloudDemo/src/main/java/com/fedex/ground/cloud/SpringCloudDemo.java
+++ SpringCloudDemo/src/main/java/com/fedex/ground/cloud/SpringCloudDemo.java
@@ -3,19 +3,24 @@ package com.fedex.ground.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableEurekaClient
 public class SpringCloudDemo {
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudDemo.class, args);
 	}
 	
-	@RequestMapping("/")
-    public String home() {
-        return "Hello world";
-    }
 }
