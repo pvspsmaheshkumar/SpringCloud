40061524acfac7091942e4a86999878218e8acc5
5099208 <5099208@PGH-L64966NB.corp.ds.fedex.com>
2019-08-25 20:18:34 -0400

    Initial Commit

---
+++ CalendarService/src/main/java/com/fedex/ground/cloud/beans/DateTimeModel.java
@@ -0,0 +1,24 @@
+package com.fedex.ground.cloud.beans;
+
+public class DateTimeModel {
+
+	public String currentDate;
+	public String currentTime;
+
+	public String getCurrentDate() {
+		return currentDate;
+	}
+
+	public void setCurrentDate(String currentDate) {
+		this.currentDate = currentDate;
+	}
+
+	public String getCurrentTime() {
+		return currentTime;
+	}
+
+	public void setCurrentTime(String currentTime) {
+		this.currentTime = currentTime;
+	}
+
+}
